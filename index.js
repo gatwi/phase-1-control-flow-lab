043a32a3316a043c7b4ff7cee1118e979d2cc284
gatwi <faith.murithi@student.moringaschool.com>
2022-06-10 11:33:35 +0300

    commit to master

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return ("This one is on me!");
+  } 
+  else if (1999 < feet && feet < 2500) {
+    // distance covered is more than 2000 feet but less than 2500 feet
+    return ("I will gladly take your thirty bucks.");
+  }
+  else (feet > 2500); {
+    // no rides above 2500 feet
+    return ("No can do.");
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+  
+function ternaryCheckCity(city) {
+  return city == "NYC" ? "Ok, sounds good.": "No go.";
+  // city of choice here is NYC
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  // Re-read again on switch functions!
+  switch (tip) {
+    case ("generous"):
+      return ("Thank you so much.");
+  }
+  switch (tip) {
+    case ("not as generous"):
+      return ("Thank you.");
+    default: 
+    return ("Bye.");
+  }
 }
